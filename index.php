65e6926367ce20324905d55945c50d78770c493d
Your Na <email@emila.com>
2013-02-22 19:49:54 +0000

    added card styles

--- index.php
+++ index.php
@@ -1,18 +1,32 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
 <html>
 <head>
-
+  <title>Monte Carlo Poker Simulation</title>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script type="text/javascript" src="main.js"></script>
 </head>
 <body>
 
+<div id="container"><div>
+  <?php
+    $output = shell_exec("./poker");
+    preg_match("/Random Hand: (.+?)\n/si", $output, $matches);
+    preg_match_all("/\w\w/si", $matches[1], $matches);
+    print "<ul class='clearfix'>";
+    foreach($matches[0] as $card) {
+      $class = substr($card, -1)." ";
+      $class .= "c".substr($card, 0, 1);
+      print "<li class='card {$class}'></li>";
+    }
+    print "</ul>";
+    print "<pre>".$output."</pre>";
+    
+  ?>
+</div></div>
+
 </body>
 </html>
-<?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-print "hello worl";
-
-print "<pre>".shell_exec("./scan")."</pre>";
-
-?>
