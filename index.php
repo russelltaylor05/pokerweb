9317c40c2fc757cb5ecec67d21c4f5a480012000
EC2 Default User <ec2-user@ip-10-16-23-55.ec2.internal>
2013-02-22 18:47:08 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+
+</head>
+<body>
+
+</body>
+</html>
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+print "hello worl";
+
+print "<pre>".shell_exec("./scan")."</pre>";
+
+?>
